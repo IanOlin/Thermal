b3061bfcabb7d27af3adcdcb34dd5c33fc927045
apan64 <andrew.pan@students.olin.edu>
2015-10-27 16:16:56 -0400

    liquid heat loss, did not find liquid to air coefficeint so only conduction loss

--- liquidHeatLoss.m
+++ liquidHeatLoss.m
@@ -1,3 +1,18 @@
-function shit = liquidHeatLoss(liquidTemp, liquidMass)
-        shit = 0;
+function shit = liquidHeatLoss(liquidEnergy, liquidMass, liquidVolume)%needs temp, mass, volume of cider
+        airSA = (.04^2 * pi) - (.02^2 * pi); %surface area in contact with air
+        mugSA = (.04^2 * pi) + (liquidVolume * 2 / .04);%contact area with mug
+        mugThickness = .007;
+        thermalConductivity = 1.5;
+        specificHeat = 4186;
+        conduction = thermalConductivity * mugSA * (energyToTemperature(liquidEnergy, liquidMass, specificHeat) - 290) / mugThickness;
+        convection = 0;%didn't find liquid to air coefficient
+        
+        shit = conduction + convection;
+end
+     function res = energyToTemperature(U, m, c)
+        res = U / heatCapacity(m,c);
     end
+    function res = heatCapacity(mass, specificHeat)
+        res = mass * specificHeat;
+    end
+    
