ec2bc5cdfe4f1797f5c1d94b56a18b3a869fe938
apan64 <andrew.pan@students.olin.edu>
2015-10-27 22:49:42 -0400

    liquid heat loss should run on ode, thermal params

--- liquidHeatLoss.m
+++ liquidHeatLoss.m
@@ -1,14 +1,18 @@
-function res = liquidHeatLoss(liquidEnergy, liquidMass, liquidVolume, params)%needs temp, mass, volume of cider
+function res = liquidHeatLoss(~, params)%params + liquidEnergy, liquidMass, liquidVolume
         airSA = (.04^2 * pi) - (.02^2 * pi); %surface area in contact with air
         mugSA = (.04^2 * pi) + (liquidVolume * 2 / .04);%contact area with mug
         mugThickness = params(9);
         thermalConductivity = params(8);
         specificHeatLiquid = params(5);
+        liquidEnergy = params(18);
+        liquidMass = params(19);
+        liquidMass = params(20);
         thermalHeatCoefficient = 10;%shitshitshitshitshitshitshitshitshitshitshitshit
         conduction = thermalConductivity * mugSA * (energyToTemperature(liquidEnergy, liquidMass, specificHeatLiquid) - 290) / mugThickness;
         convection = thermalHeatCoefficient * airSA * (energyToTemperature(liquidEnergy, liquidMass, specificHeatLiquid) - 290);
-        
-        res = conduction + convection;
+        newParams = zeros(1, 21);
+        newParams(18) = conduction + convection;
+        res = newParams;
 end
 
      function res = energyToTemperature(U, m, c)
--- thermal.m
+++ thermal.m
@@ -72,7 +72,13 @@ function thermal()
         params(15) = heatOfVaporization;
         params(16) = massOfSteam;
         params(17) = steamEnergy;
+        params(18) = liquidEnergy;%this currently = stuff, need to establish actual energy value
+        params(19) = liquidMass;
+        params(20) = liquidVolume;
+        params(21) = barEnergy;
         
+        
+
         %% main
         for n = initialTime:finalTime
             barToSteam(barTemp, steamTemp, steamMass);
