31868d814853b4e1a57287cc3d8bf62c49e75dbe
IanOlin <ian.paul@students.olin.edu>
2015-10-28 01:13:42 -0400

    odefi

--- steamToLiquid.m
+++ steamToLiquid.m
@@ -1,11 +1,18 @@
-function res = steamToLiquid(steamEnergy, liquidEnergy, steamMass, liquidMass, params)
+function res = steamToLiquid(~, params)
     specificHeatLiquid = params(5);
     specificHeatSteam = params(6);
     steamSA = params(11);
     steamLiquidCoefficient = params(12);
+    steamEnergy = params(17);
+    liquidEnergy = params(18);
+    steamMass = params(16);
+    liquidMass = params(19);
     deltaTemp = energyToTemperature(steamEnergy, steamMass, specificHeatSteam) - energyToTemperature(liquidEnergy, liquidMass, specificHeatLiquid);
     flow = steamLiquidCoefficient * steamSA * deltaTemp; 
-    res = flow;
+    newParams = zeros(1, length(params));
+    newParams(18) = flow;
+    newParams(17) = -flow;
+    res = newParams.';
 end
     function res = energyToTemperature(U, m, c)
         res = U / heatCapacity(m,c);
