7922b8b13b3964b1a5f9ee3b239527855e8a80c9
IanOlin <ian.paul@students.olin.edu>
2015-10-27 23:02:46 -0400

    ODE -ifyed

--- barToSteam.m
+++ barToSteam.m
@@ -1,14 +1,18 @@
-function res = barToSteam(heat, steamEnergy, massSteam, params)%current heat of bar, steam temp, non ode runnable
+function res = barToSteam(~, params)%current heat of bar, steam temp, non ode runnable
     transferCoefficient = parmas(13); %this is shit
     specificHeatBar = params(7) ; %specific heat in joules per kg kelvin
     specificHeatSteam = params(6);%%specific heat in joules per kg kelvin
     massBar = params(14); %mass
-    surfaceAreaBar = params(4);    
-        deltaTemperature = energyToTemperature(heat, massBar, specificHeatBar) -energyToTemperature(steamEnergy, massSteam, specificHeatSteam);
-        
-        conductionFlow = transferCoefficient * surfaceAreaBar * deltaTemperature;%flow of heat
-        
-        res = conductionFlow;
+    surfaceAreaBar = params(4);   
+    heatBar = params(21);
+    steamEnergy = params(17);
+    massSteam = params(16);
+    deltaTemperature = energyToTemperature(heatBar, massBar, specificHeatBar) -energyToTemperature(steamEnergy, massSteam, specificHeatSteam);
+    conductionFlow = transferCoefficient * surfaceAreaBar * deltaTemperature;%flow of heat
+    newParams = zeros(1, length(params));
+    newParams(21) = -conductionFlow;
+    newParams(17) = conductionFlow;
+    res = conductionFlow;
 end
     function res = energyToTemperature(U, m, c)
         res = U / heatCapacity(m,c);
